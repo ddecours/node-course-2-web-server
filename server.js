62bc3efbff5d0394ffc7822861e6beb591a392bf
Doug DeCoursin <doug_decoursin@hotmail.com>
2019-01-17 19:51:49 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.set('view engine', 'hbs');
+
+
+app.use( (request, response, next) => {
+    var now = new Date();
+    var myDateString = now.getFullYear() + '-' + (now.getMonth()+1) + '-' + now.getDate();
+    var logMessage = `${now.toString()}: ${request.method} ${request.url}` + '\n';
+    var logFilename = 'server.log' + "_" + myDateString + '.log';
+
+    console.log(logMessage);
+    fs.appendFile(logFilename, logMessage, (err) => {
+        if (err) {
+            console.log(`Unable to append to log file: ${logFilename}`);
+        };
+    });
+    next();
+});
+// ONLY NEEDED WHEN SITE UNDER MAINTENANCE
+// app.use( (request, response, next) => {
+//     response.render('maintenance.hbs', {
+//         pageTitle: 'Maintenance Page',
+//         maintenanceMessage: 'Site currently under maintenance - please check back later'
+//     });
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+    response.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to our web site '
+    });
+});
+
+app.get('/about', (request, response) => {
+    response.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (request, response) => {
+    response.send({
+        errorMessage: 'You stink!'
+    });
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
